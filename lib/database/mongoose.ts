3620b6d6db1dbf3065c43467abd22a8f7c958822
Christian Vivas <160914543+ChrisVivasAI@users.noreply.github.com>
2024-05-24 19:13:52 -0400

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'socialsizer', bufferCommands: false 
+      dbName: 'social', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
